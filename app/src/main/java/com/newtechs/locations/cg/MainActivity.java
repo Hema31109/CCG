c975477f81d6e6a339fddb9cc9f072be41231c8d
RubiniMadhasen <34648478+RubiniMadhasen@users.noreply.github.com>
2018-05-08 13:39:52 +0530

    driver,splash,user.....

--- app/src/main/java/com/newtechs/locations/cg/MainActivity.java
+++ app/src/main/java/com/newtechs/locations/cg/MainActivity.java
@@ -1,13 +1,34 @@
 package com.newtechs.locations.cg;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView=findViewById(R.id.image);
+       Thread thread=new Thread(){
+            @Override
+            public void run() {
+                 try {
+                    sleep(4000);
+                    Intent intent=new Intent(getApplicationContext(),splashscreen.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
     }
-}
+
+    }
+
--- app/src/main/java/com/newtechs/locations/cg/Police.java
+++ app/src/main/java/com/newtechs/locations/cg/Police.java
@@ -3,6 +3,8 @@ package com.newtechs.locations.cg;
 import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
@@ -10,30 +12,41 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class Police extends AppCompatActivity {
-    ListView listView;
-    ArrayList arrayList;
-    Button click;
+    RecyclerView recyclerView;
+    ArrayList arrayList1,arrayList2,arrayList3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_police);
-        listView=findViewById(R.id.list);
-        arrayList=new ArrayList();
-        arrayList.add("hai");
-        arrayList.add("hai0");
-        arrayList.add("hai");
-        arrayList.add("hai0");
-        Custom custom=new Custom(Police.this,arrayList);
-        listView.setAdapter(custom);
-        click=findViewById(R.id.click);
-        click.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog.Builder alert = new AlertDialog.Builder(Police.this);
-                View view=getLayoutInflater().inflate(R.layout.dialog,null);
-                alert.setView(view);
-                alert.show();
-            }
-        });
+        arrayList1=new ArrayList();
+        arrayList2=new ArrayList();
+        arrayList3=new ArrayList();
+        recyclerView=findViewById(R.id.recycler);
+        for (int i=0;i<=5;i++)
+        {
+            arrayList1.add(i+1);
+        }
+        arrayList2.add("Rajhasthan");
+        arrayList2.add("Delhi");
+        arrayList2.add("Mumbai");
+        arrayList2.add("Rajhasthan");
+        arrayList2.add("Rajhasthan");
+        arrayList2.add("Rajhasthan");
+
+
+        arrayList3.add("11:00");
+        arrayList3.add("09:00");
+        arrayList3.add("07:00");
+        arrayList3.add("07:00");
+        arrayList3.add("07:00");
+        arrayList3.add("07:00");
+
+        MyRecycler myRecycler=new MyRecycler(Police.this,arrayList1,arrayList2,arrayList3);
+        recyclerView.setAdapter(myRecycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+//                       AlertDialog.Builder alert = new AlertDialog.Builder(Police.this);
+//                View view=getLayoutInflater().inflate(R.layout.dialog,null);
+//                alert.setView(view);
+//                alert.show();
     }
 }
